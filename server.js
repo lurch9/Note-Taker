6ccc019266d3b8b5e43c5b43a2d96035274a8c3e
lurch9 <90292267+lurch9@users.noreply.github.com>
2021-12-09 17:36:47 -0800

    added get routes for index and notes

--- server.js
+++ server.js
@@ -4,10 +4,19 @@ const fs = require('fs');
 const uuid = require('./helpers/uuid');
 const PORT = process.env.PORT || 3002;
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.redirect('index.html');
+});
+
+app.get('/', (req, res) => {
+    res.redirect('notes.html');
+});
+
 app.listen(PORT, () => {
     console.log(`Server Up in Port ${PORT}`);
-})
+});
