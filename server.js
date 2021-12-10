dbda5fb7b77ea439fb85f683eb1cd2373e2c495b
lurch9 <90292267+lurch9@users.noreply.github.com>
2021-12-09 21:14:27 -0800

    Wrote and debugged html and api routes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const paths = require('paths');
+const path = require('path');
 const fs = require('fs');
 const uuid = require('./helpers/uuid');
 const PORT = process.env.PORT || 3002;
@@ -9,15 +9,56 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.redirect('index.html');
+    app.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, './public/notes.html'));
+    });
+
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, './public/index.html'))
+    });
+
+
+app.get('/api/notes', (req, res) => {
+    console.log("Reading notes...");
+    try {
+        
+        const db = fs.readFileSync('./db/db.json', 'utf-8');
+        const notes = JSON.parse(db || [])
+        res.json(notes);
+    }
+    catch (error){
+        console.log(`Something isn\'t right: ${error}`);
+        res.json(error); 
+    }
 });
 
-app.get('/', (req, res) => {
-    res.redirect('notes.html');
-});
+app.post('/api/notes', (req, res) => {
 
+    if (req.body) {
+        let newNote = req.body;
+        newNote.id = uuid();
+        const jsonNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+        
+        try{
+            jsonNotes.push(newNote);
+            fs.writeFileSync('./db/db.json', JSON.stringify(jsonNotes));
+            
+        }
+        catch (error){
+            console.log(`Something went wrong! Error: ${error}`);
+            res.json(error);
+        }
+        res.json(jsonNotes);
+        }
 
+        else{
+
+        console.log('Title or Text empty.')
+    
+    }
+
+    
+})
 
 app.listen(PORT, () => {
     console.log(`Server Up in Port ${PORT}`);
